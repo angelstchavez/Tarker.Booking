891d0a14b2dfb1ead0e4f271d8510b711a78f80e
angelstchavez <angelstchavez@gmail.com>
2024-02-08 17:52:24 -0500

    feat: add enums and entities into domain

---
+++ Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Tarker.Booking.Domain.Entities.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public required string FullName { get; set; }
+        public required string DocumentNumber { get; set; }
+    }
+}
---
+++ Tarker.Booking.Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Tarker.Booking.Domain.Entities.User
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
