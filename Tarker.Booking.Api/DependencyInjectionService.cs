8033d6f31ad0181a2e4bdb70d85a13b681c29809
angelstchavez <angelstchavez@gmail.com>
2024-02-09 05:43:04 -0500

    feat: integrate Swagger documentation

--- Tarker.Booking.Api/DependencyInjectionService.cs
+++ Tarker.Booking.Api/DependencyInjectionService.cs
@@ -1,9 +1,37 @@
-﻿namespace Tarker.Booking.Api
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Tarker.Booking.Api
 {
     public static class DependencyInjectionService
     {
         public static IServiceCollection AddWebApi(this IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Tarker Booking API",
+                    Description = "API para gestionar reservas en el sistema Tarker.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Equipo de desarrollo de Tarker",
+                        Email = "contacto@tarker.com",
+                        Url = new System.Uri("https://www.tarker.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licencia MIT",
+                        Url = new System.Uri("https://opensource.org/licenses/MIT")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
             return services;
         }
     }
--- Tarker.Booking.Api/Program.cs
+++ Tarker.Booking.Api/Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 app.MapControllers();
 
 app.Run();
