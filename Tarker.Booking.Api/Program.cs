7abf19c6eeece3dfafbd196665d92aec86f7b0d6
angelstchavez <angelstchavez@gmail.com>
2024-02-09 00:34:28 -0500

    feat: add dependency injection services

--- Tarker.Booking.Api/Program.cs
+++ Tarker.Booking.Api/Program.cs
@@ -1,13 +1,17 @@
-using Microsoft.EntityFrameworkCore;
-using Tarker.Booking.Application.Interfaces;
-using Tarker.Booking.Persistence.Database;
+using Tarker.Booking.Api;
+using Tarker.Booking.Application;
+using Tarker.Booking.Common;
+using Tarker.Booking.External;
+using Tarker.Booking.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DatabaseService>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
-
-builder.Services.AddScoped<IDatabaseService, DatabaseService>();
+builder.Services
+    .AddWebApi()
+    .AddCommon()
+    .AddApplication()
+    .AddExternal(builder.Configuration)
+    .AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Tarker.Booking.Persistence/DependencyInjectionService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tarker.Booking.Application.Interfaces;
+using Tarker.Booking.Persistence.Database;
+
+namespace Tarker.Booking.Persistence
+{
+    public static class DependencyInjectionService
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<DatabaseService>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SQLConnection")));
+
+            services.AddScoped<IDatabaseService, DatabaseService>();
+
+            return services;
+        }
+    }
+}
